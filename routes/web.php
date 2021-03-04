a39416e35b8fb2151be45a68b2c64ef0837d78f2
kereszti.krisztian <kereszti.krisztian@marktsoft.hu>
2021-03-04 18:52:49 +0100

    Replace main to static index html

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+// Also can be used:
+// Route::view('/', 'index');
+
