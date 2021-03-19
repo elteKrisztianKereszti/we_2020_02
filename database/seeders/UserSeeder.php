7cd25a6fca6ea50fc3ff47260458bdd0e0389770
kereszti.krisztian <kereszti.krisztian@marktsoft.hu>
2021-03-19 13:29:08 +0100

    Create 1-N between Project<->User

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,10 +4,13 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder {
   public function run() {
+    DB::table('users')->truncate();
+
     User::create([
       'name' => 'Joe Smith',
       'email' => 'joe@gmail.com',
