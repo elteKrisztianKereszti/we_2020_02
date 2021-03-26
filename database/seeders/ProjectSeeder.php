6a9d1cef800247b3bbe4b5945a81c68b7158ca19
kereszti.krisztian <kereszti.krisztian@marktsoft.hu>
2021-03-26 12:53:53 +0100

    temp usare assign for project

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\Filter;
 use App\Models\Project;
 use App\Models\Track;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
@@ -20,8 +21,19 @@ class ProjectSeeder extends Seeder
         DB::table('projects')->truncate();
 
         $filters = Filter::all();
+        $users = User::all();
 
-        Project::factory(4)->create()->each(function ($project) use ($filters) {
+        Project::factory(4)->for($users[0])->create()->each(function ($project) use ($filters) {
+            $project->tracks()->createMany(
+                Track::factory(5)->make()->toArray()
+                )->each(function ($track) use ($filters) {
+                    for ($i = 0; $i < random_int(1, 4); $i++) {
+                        $track->filters()->toggle($filters[random_int(0, 9)]);
+                    }
+            });
+        });
+
+        Project::factory(3)->for($users[1])->create()->each(function ($project) use ($filters) {
             $project->tracks()->createMany(
                 Track::factory(5)->make()->toArray()
                 )->each(function ($track) use ($filters) {
